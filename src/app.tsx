7673fc60054e328c12cc132d18ee145fc6f891e6
Mario <eternal.git@gmail.com>
2024-04-26 16:12:57 +0100

    feat(home): new car image, progress on internals

--- src/app.tsx
+++ src/app.tsx
@@ -1,49 +1,70 @@
+import { useEffect } from 'react';
 import { FaCheck } from 'react-icons/fa';
 import Badge from '~/components/badge';
 import Header from '~/components/header';
+import HueSlider from '~/components/hue-slider';
 
 function App() {
+	useEffect(() => {
+		function onKeyDown(event: KeyboardEvent) {
+			if (event.key === 'F7') {
+
+			}
+		}
+
+		document.addEventListener('keydown', onKeyDown);
+		return () => document.removeEventListener('keydown', onKeyDown);
+	}, []);
+
 	return <div className='flex flex-col'>
 		<Header />
-		<div className='container flex flex-col w-auto'>
-			<h5 className='text-5xl font-bold'>
-				Aquarius Driving
-			</h5>
-			<div className='flex w-full flex-col gap-2 justify-center'>
-				<span className='flex items-center gap-2 text-lg font-semibold'>
-					<Badge>
-						<FaCheck size={14} />
-					</Badge>
-					DVSA Approved
-				</span>
-				<span className='flex items-center gap-2 text-lg font-semibold'>
-					<Badge>
-						<FaCheck size={14} />
-					</Badge>
-					DBS Checked
-				</span>
-				<span className='flex items-center gap-2 text-lg font-semibold'>
-					<Badge>
-						<FaCheck size={14} />
-					</Badge>
-					Competitive Prices
-				</span>
-				<span className='flex items-center gap-2 text-lg font-semibold'>
-					<Badge>
-						<FaCheck size={14} />
-					</Badge>
-					Discounted Block Bookings
-				</span>
-				<span className='flex items-center gap-2 text-lg font-semibold'>
-					<Badge>
-						<FaCheck size={14} />
-					</Badge>
-					Mock tests
-				</span>
+		<div className='container flex flex-col md:align-middle w-full gap-12 mt-20'>
+			<div className='flex items-stretch justify-between'>
+				<div className='flex flex-col gap-12 '>
+					<h5 className='text-5xl font-bold'>
+						Getting you in gear.
+					</h5>
+					<div className='flex w-full flex-col gap-2 justify-center'>
+						<span className='flex items-center gap-2 text-lg font-semibold'>
+							<Badge>
+								<FaCheck size={14} />
+							</Badge>
+							DVSA Approved
+						</span>
+						<span className='flex items-center gap-2 text-lg font-semibold'>
+							<Badge>
+								<FaCheck size={14} />
+							</Badge>
+							DBS Checked
+						</span>
+						<span className='flex items-center gap-2 text-lg font-semibold'>
+							<Badge>
+								<FaCheck size={14} />
+							</Badge>
+							Competitive Prices
+						</span>
+						<span className='flex items-center gap-2 text-lg font-semibold'>
+							<Badge>
+								<FaCheck size={14} />
+							</Badge>
+							Discounted Block Bookings
+						</span>
+						<span className='flex items-center gap-2 text-lg font-semibold'>
+							<Badge>
+								<FaCheck size={14} />
+							</Badge>
+							Mock Tests
+						</span>
+					</div>
+				</div>
+				<img
+					width='500em'
+					src='/img/car.png'
+					className='hue-rotate-[var(--hue)]'
+				/>
 			</div>
-			<div className='w-full overflow-visible'>
-				<div id='hero' />
-				<img width='500em' src='/img/car.png' />
+			<div>
+				<HueSlider />
 			</div>
 		</div>
 	</div>;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,10 @@
 function Header() {
 	return <nav className='sticky h-18 flex px-5 py-3 items-center'>
-		<div className='container flex h-14 items-center gap-4 p-0'>
+		<div className='container flex h-14 items-center gap-6 p-0'>
+			<h5 className='hidden md:block text-xl font-bold'>
+				Aquarius Driving
+			</h5>
+			<div className='hidden md:block w-[1px] h-4 opacity-25 bg-black' />
 			<div className='flex gap-6 font-semibold'>
 				<a className='animate-underline' href='#home'>Home</a>
 				<a className='animate-underline' href='#contact'>Contact Us</a>
