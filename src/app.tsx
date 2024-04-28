e2522de0e0b23312953b00bf135fbf9603c2c6aa
Mario <me@marioparaschiv.com>
2024-04-28 16:09:32 +0100

    finalize design

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { FaMountainSun, FaCheck, FaArrowsLeftRight } from 'react-icons/fa6';
+import { FaMountainSun, FaCheck, FaArrowsLeftRight, FaAddressCard } from 'react-icons/fa6';
 import { FaPoundSign } from 'react-icons/fa';
 import Contact from '~/components/contact';
 import Header from '~/components/header';
@@ -34,22 +34,24 @@ function App() {
 				/>
 			</div>
 			<div className='h-[1px] w-11/12 bg-background-secondary mx-auto' />
-			<div id='our-offer' className='flex flex-col md:flex-row gap-6 text-secondary-fg text-center h-full'>
+			<div id='about' className='flex flex-col md:flex-row gap-6 text-secondary-fg text-center h-full'>
 				<div className='flex-col gap-4 min-w-64 p-6 min-h-64  flex items-center w-full h-full justify-start'>
 					<FaMountainSun className='text-brand' size={64} />
 					<h2 className='text-3xl sm:text-4xl text-primary-fg font-semibold'>
 						Wide Coverage
 					</h2>
-					<span className='text-pretty'>
+					<span className='text-pretty md:text-lg'>
 						We offer lessons across multiple locations, including but not limited to <b>Enfield, Lower Edmonton, Palmers Green, Winchmore Hill, Oakwood, Southgate, and Barnet.</b> We provide door-to-door service, with options to pick you up from school or work for maximum convenience.
 					</span>
 				</div>
 				<div className='flex-col gap-4 min-w-64 p-6 min-h-64 flex items-center w-full h-full justify-start'>
-					<FaPoundSign className='pound-icon text-brand' size={64} />
+					<div className='pound-icon'>
+						<FaPoundSign className='text-brand' size={64} />
+					</div>
 					<h2 className='text-3xl sm:text-4xl text-primary-fg font-semibold'>
 						Affordable
 					</h2>
-					<span className='text-pretty'>
+					<span className='text-pretty md:text-lg'>
 						Our lessons are competitively priced to suit any budget. We offer flexible lesson durations and packages, whether you're a beginner or need refresher courses. Quality instruction without breaking the bank.
 					</span>
 				</div>
@@ -58,7 +60,7 @@ function App() {
 					<h2 className='text-3xl sm:text-4xl text-primary-fg font-semibold'>
 						Flexible
 					</h2>
-					<span className='text-pretty'>
+					<span className='text-pretty md:text-lg'>
 						Learn at your own pace with flexible scheduling and personalized instruction. Choose from 1-hour, 1.5-hour, or 2-hour sessions. We are patient and understanding and create a calm environment to build your driving confidence.
 					</span>
 				</div>
@@ -67,9 +69,23 @@ function App() {
 			<Contact />
 			<div className='h-[1px] w-11/12 bg-background-secondary mx-auto' />
 			<div className='flex items-center justify-center gap-2 flex-col'>
-				© {new Date().getFullYear()}
+				<div className='flex flex-col items-center justify-center gap-4'>
+					<div className='flex flex-col gap-4 justify-between items-center'>
+						<div className='bg-brand w-fit text-white rounded-full p-4'>
+							<FaAddressCard size={24} />
+						</div>
+						<a className='animate-underline font-semibold text-secondary-fg hover:text-primary-fg' href='tel:+447940484023'>
+							+44 7940484023
+						</a>
+						<a className='animate-underline text-sm font-semibold text-secondary-fg hover:text-primary-fg' href='tel:+447940484023'>
+							aquariusdrivingtuition@hotmail.com
+						</a>
+					</div>
+				</div>
+				{/* <span>aquariusdrivingtuition@hotmail.com</span> */}
+				<span className='h-[1px] bg-background-secondary w-24 my-4' />
 				<a className='animate-underline' href='https://marioparaschiv.com'>Valentin-Mario Paraschiv</a>
-				All Rights Reserved.
+				© {new Date().getFullYear()} | All Rights Reserved.
 			</div>
 		</div>
 	</div>;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
 function Header() {
 	return <nav className='sticky h-18 flex px-5 py-3 items-center border-b md:border-none'>
-		<div className='container flex h-14 items-center justify-center md:justify-start gap-6 p-0 font-semibold'>
+		<div className='container flex h-14 items-center justify-center md:justify-start gap-6 p-0 font-semibold text-secondary-fg text-sm md:text-base'>
 			{/* <h5 className='hidden md:block text-xl font-bold'>
 				Aquarius Driving
 			</h5>
 			<div className='hidden md:block w-[1px] h-4 opacity-25 bg-black' /> */}
-			<div className='flex gap-6 text-secondary-fg'>
+			<div className='flex gap-6'>
 				<a className='animate-underline hover:text-primary-fg' href='#home'>Home</a>
-				<a className='animate-underline hover:text-primary-fg' href='#our-offer'>Our Offer</a>
-				<a className='animate-underline hover:text-primary-fg' href='#contact'>Contact Us</a>
+				<a className='animate-underline hover:text-primary-fg' href='#about'>About</a>
+				<a className='animate-underline hover:text-primary-fg' href='#contact'>Contact</a>
 			</div>
 		</div>
 	</nav>;
