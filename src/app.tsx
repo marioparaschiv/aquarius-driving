bb5174e09262b37af89aeb895c32fb1c8ff0961c
Mario <me@marioparaschiv.com>
2024-04-28 22:51:23 +0100

    FIXES

--- src/app.tsx
+++ src/app.tsx
@@ -3,25 +3,35 @@ import { FaPoundSign } from 'react-icons/fa';
 import Contact from '~/components/contact';
 import Header from '~/components/header';
 import Badge from '~/components/badge';
+import { cn } from '~/utils';
 
 function App() {
 	return <div id='home' className='flex flex-col mb-10'>
 		<Header />
 		<div className='container flex flex-col md:align-middle w-full gap-12 mt-10 md:mt-20'>
 			<div className='flex items-center md:items-stretch gap-6 justify-center md:justify-between flex-col-reverse md:flex-row'>
+				<div className='flex text-sm md:hidden w-full flex-col gap-2 justify-center'>
+					<Benefit name='DVSA Approved Female Instructor' />
+					<Benefit name='DBS Checked' />
+					<Benefit name='Competitive Prices' />
+					<Benefit name='Manual Car' />
+					<Benefit name='Discounted Block Bookings' />
+					<Benefit name='Mock Tests' />
+				</div>
 				<div className='flex flex-col gap-12'>
 					<div className='flex flex-col gap-2 text-center md:text-start'>
 						<h1 className='text-4xl sm:text-5xl font-semibold flex items-center gap-4'>
-							Aquarius Driving School
+							Aquarius Driving Tuition
 						</h1>
 						<h2 className='text-2xl sm:text-3xl text-secondary-fg font-semibold'>
 							Getting you in gear.
 						</h2>
 					</div>
 					<div className='hidden md:flex w-full flex-col gap-2 justify-center'>
-						<Benefit name='DVSA Approved' />
+						<Benefit name='DVSA Approved Female Instructor' />
 						<Benefit name='DBS Checked' />
 						<Benefit name='Competitive Prices' />
+						<Benefit name='Manual Car' />
 						<Benefit name='Discounted Block Bookings' />
 						<Benefit name='Mock Tests' />
 					</div>
@@ -30,7 +40,7 @@ function App() {
 					src='/img/car.png'
 					loading='lazy'
 					decoding='sync'
-					className='select-none w-[500px] lg:w-[700px] object-contain hue-rotate-[var(--hue)]'
+					className='select-none md:w-[500px] lg:w-[700px] object-contain hue-rotate-[var(--hue)]'
 				/>
 			</div>
 			<div className='h-[1px] w-11/12 bg-secondary mx-auto' />
@@ -41,7 +51,7 @@ function App() {
 						Wide Coverage
 					</h2>
 					<span className='text-pretty md:text-lg'>
-						We offer lessons across multiple locations, including but not limited to <b>Enfield, Lower Edmonton, Palmers Green, Winchmore Hill, Oakwood, Southgate, and Barnet.</b> We provide door-to-door service, with options to pick you up from school or work for maximum convenience.
+						We offer manual lessons across multiple locations, including but not limited to <b>Enfield, Lower Edmonton, Palmers Green, Winchmore Hill, Oakwood, Southgate, and Barnet.</b> We provide door-to-door service, with options to pick you up from school or work for maximum convenience.
 					</span>
 				</div>
 				<div className='flex-col gap-4 min-w-64 p-6 min-h-64 flex items-center w-full h-full justify-start'>
@@ -52,7 +62,7 @@ function App() {
 						Affordable
 					</h2>
 					<span className='text-pretty md:text-lg'>
-						Our lessons are competitively priced to suit any budget. We offer flexible lesson durations and packages, whether you're a beginner or need refresher courses. Quality instruction without breaking the bank.
+						Our manual lessons are competitively priced to suit most budgets. We offer flexible lesson durations and packages, whether you're a beginner or need refresher courses. Quality instruction without breaking the bank.
 					</span>
 				</div>
 				<div className='flex-col gap-4 min-w-64 p-6 min-h-64  flex items-center w-full h-full justify-start'>
@@ -61,7 +71,7 @@ function App() {
 						Flexible
 					</h2>
 					<span className='text-pretty md:text-lg'>
-						Learn at your own pace with flexible scheduling and personalized instruction. Choose from 1-hour, 1.5-hour, or 2-hour sessions. We are patient and understanding and create a calm environment to build your driving confidence.
+						Learn at your own pace with flexible scheduling and personalized instruction. Choose from 1-hour, 1.5-hour, or 2-hour sessions. We are patient and understanding and create a calm environment to build your driving confidence & skills.
 					</span>
 				</div>
 			</div>
@@ -82,7 +92,6 @@ function App() {
 						</a>
 					</div>
 				</div>
-				{/* <span>aquariusdrivingtuition@hotmail.com</span> */}
 				<span className='h-[1px] bg-border w-24 my-4' />
 				<a className='animate-underline' href='https://marioparaschiv.com'>Valentin-Mario Paraschiv</a>
 				© {new Date().getFullYear()} | All Rights Reserved.
@@ -91,13 +100,13 @@ function App() {
 	</div>;
 }
 
-function Benefit({ name }: { name: string; }) {
-	return <span className='flex items-center gap-2 text-lg font-medium'>
+function Benefit({ name, ...props }: { name: string; } & React.ComponentProps<'span'>) {
+	return <span {...props} className={cn(props.className, 'flex items-center gap-2 text-lg font-medium')}>
 		<Badge>
 			<FaCheck size={14} />
 		</Badge>
 		{name}
-	</span>;
+	</ span>;
 }
 
-export default App;
+export default App;;
